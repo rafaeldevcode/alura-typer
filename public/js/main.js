def3bc553603c285c4b9f2168c55399d33ad3dc5
Rafael Vieira <rafaeldevcode@gmail.com>
2022-03-31 21:24:43 -0300

    Finalizando modulo 1, 2 e 3

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,53 @@
-var frase = $('.frase').text();
-var numeroPalavras = $('#numero-palavras');
-var palavras = frase.split(' ').length;
-    numeroPalavras.text(palavras);
+var campo = $('.texto-digitado')
+var tempoInicial = $('#tempo-digitacao').text();
+
+$(document).ready(()=>{
+    contarPalavras();
+    contarPalavrasDigitadas();
+    iniciarContador();
+    $('#reiniciar-jogo').click(reiniaciarJogo);
+})
+
+function contarPalavras(){
+    let frase = $('.frase').text();
+    let numeroPalavras = $('#qtd-palavras');
+    let qtdPalavras = frase.split(' ').length;
+        numeroPalavras.text(qtdPalavras);
+}
+
+function contarPalavrasDigitadas(){
+    campo.on('input', ()=>{
+        let conteudo = campo.val();
+
+        let qtdPalavras = conteudo.split(/\S+/).length - 1;
+            $('#qtd-palavras-dig').text(qtdPalavras);
+
+        let qtdCaracteres = conteudo.length;
+            $('#qtd-caracteres').text(qtdCaracteres);
+    })
+}
+
+function iniciarContador(){
+    let temposRestante = $('#tempo-digitacao').text();
+
+    campo.one('focus', ()=>{
+        let id = setInterval(()=>{
+            temposRestante--;
+            $('#tempo-digitacao').text(temposRestante);
+
+            if(temposRestante < 1){
+                campo.attr('disabled', true);
+                clearInterval(id);
+            }
+        }, 1000);
+    });
+}
+
+function reiniaciarJogo(){
+    campo.attr('disabled', false);
+    campo.val('');
+    $('#qtd-palavras-dig').text('0');
+    $('#qtd-caracteres').text(0);
+    $('#tempo-digitacao').text(tempoInicial);   
+    iniciarContador();
+}
