ee57a25829c608b4e74a256e51d22fdc7b3465e5
Rafael Vieira <rafaeldevcode@gmail.com>
2022-04-05 20:02:13 -0300

    Fazendo capitulo 3 do modulo 2

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,11 +1,24 @@
 $('#mostrar-frase').click(carregarFraseAleatoria);
 
 function carregarFraseAleatoria(){
+    $('#spinner').show();
+
     $.get('http://localhost:3000/frases', (res)=>{
         let numeroAleatorio = Math.floor(Math.random() * res.length);
             
             $('.frase').text(res[numeroAleatorio].texto);
             auatizaTempoInicial(res[numeroAleatorio].tempo);
             contarPalavras();
+    }).fail((error)=>{
+        let messageErro = error.status + ' ' + error.statusText + ': ' +error.responseText;
+        let erroClass = $('.erros')
+            erroClass.show();
+            erroClass.text(messageErro);
+
+            setTimeout(() => {
+                erroClass.hide();
+            }, 2500);
+    }).always(()=>{
+        $('#spinner').hide();
     });
 }
